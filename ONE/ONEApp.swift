620c9ba99fcc3ad0bb08ad967468abc3649ce4af
neo849 <f.michi84.989@gmail.com>
2025-05-28 23:57:54 +0200

    Initial Commit

---
+++ ONE/ONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONEApp.swift
+//  ONE
+//
+//  Created by Michael Fleps on 28.05.25.
+//
+
+import SwiftUI
+
+@main
+struct ONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
